452d6c9d53215fa7a950ac8101fa418445e950f0
ZhouPeng <zp_syx@163.com>
2013-08-11 21:33:44 +0800

    统一robot_encoder代码风格。

--- Demo/CORTEX_M4F_STM32F407ZG-SK/User/robot_encoder.c
+++ Demo/CORTEX_M4F_STM32F407ZG-SK/User/robot_encoder.c
@@ -1,20 +1,21 @@
+/*****************************************************************************/
 /**
-  ******************************************************************************
-  * @file    EncoderSensor.c
-  * @author  ZhouPeng
-  * @version V0.1
-  * @date    2013-3-22
-  * @brief   This file provides a set of functions needed to manage the EncoderSensor
-  *          board via SPI interface.
-  */
+* @file robot_encoder.c
+*
+*
+* This file contains a driver which get absolute encoder value and 
+* 2.4G wireless remote control information via spi interface.
+*
+* @note
+*
+* None.
+*
+******************************************************************************/
 
-/* Includes ------------------------------------------------------------------*/
-#include "EncoderSensor.h"
+/***************************** Include Files *********************************/
+#include "robot_encoder.h"
 
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Private_Defines
-  * @{
-  */
-__IO uint32_t  EncoderSensorTimeout = EncoderSensor_FLAG_TIMEOUT;   
+/************************** Constant Definitions *****************************/
 
 /* Read/Write command */
 #define READWRITE_CMD              ((uint8_t)0x80) 
@@ -23,220 +24,229 @@ __IO uint32_t  EncoderSensorTimeout = EncoderSensor_FLAG_TIMEOUT;
 /* Dummy Byte Send by the SPI Master device in order to generate the Clock to the Slave device */
 #define DUMMY_BYTE                 ((uint8_t)0x00)
 
-/**
-  * @}
-  */
 
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Private_Macros
-  * @{
-  */
+/**************************** Type Definitions *******************************/
 
-/**
-  * @}
-  */ 
-  
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Private_Variables
-  * @{
-  */ 
+/***************** Macros (Inline Functions) Definitions *********************/
 
-/**
-  * @}
-  */
+/************************** Function Prototypes ******************************/
 
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Private_FunctionPrototypes
-  * @{
-  */
-static uint8_t EncoderSensor_SendByte(uint8_t byte);
-static void EncoderSensor_Read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead);
-/**
-  * @}
-  */
+/************************** Variable Definitions *****************************/
 
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Private_Functions
-  * @{
-  */
-
-/**
-  * @brief  Set EncoderSensor Initialization.
-  * @param  None
-  * @retval None
-  */
-void vEncoderSensorInitialise(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;
-  SPI_InitTypeDef  SPI_InitStructure;
-
-  /* Enable the SPI periph */
-  RCC_APB1PeriphClockCmd(EncoderSensor_SPI_CLK, ENABLE);
-
-  /* Enable SCK, MOSI and MISO GPIO clocks */
-  RCC_AHB1PeriphClockCmd(EncoderSensor_SPI_SCK_GPIO_CLK | EncoderSensor_SPI_MISO_GPIO_CLK | EncoderSensor_SPI_MOSI_GPIO_CLK, ENABLE);
-
-  /* Enable CS  GPIO clock */
-  RCC_AHB1PeriphClockCmd(EncoderSensor_SPI_CS_GPIO_CLK, ENABLE);
-  
-  GPIO_PinAFConfig(EncoderSensor_SPI_SCK_GPIO_PORT, EncoderSensor_SPI_SCK_SOURCE, EncoderSensor_SPI_SCK_AF);
-  GPIO_PinAFConfig(EncoderSensor_SPI_MISO_GPIO_PORT, EncoderSensor_SPI_MISO_SOURCE, EncoderSensor_SPI_MISO_AF);
-  GPIO_PinAFConfig(EncoderSensor_SPI_MOSI_GPIO_PORT, EncoderSensor_SPI_MOSI_SOURCE, EncoderSensor_SPI_MOSI_AF);
-
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-
-  /* SPI SCK pin configuration */
-  GPIO_InitStructure.GPIO_Pin = EncoderSensor_SPI_SCK_PIN;
-  GPIO_Init(EncoderSensor_SPI_SCK_GPIO_PORT, &GPIO_InitStructure);
-
-  /* SPI  MOSI pin configuration */
-  GPIO_InitStructure.GPIO_Pin =  EncoderSensor_SPI_MOSI_PIN;
-  GPIO_Init(EncoderSensor_SPI_MOSI_GPIO_PORT, &GPIO_InitStructure);
-
-  /* SPI MISO pin configuration */
-  GPIO_InitStructure.GPIO_Pin = EncoderSensor_SPI_MISO_PIN;
-  GPIO_Init(EncoderSensor_SPI_MISO_GPIO_PORT, &GPIO_InitStructure);
-
-  /* SPI configuration -------------------------------------------------------*/
-  SPI_I2S_DeInit(EncoderSensor_SPI);
-  SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-  SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-  SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
-  SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
-  SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-  SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
-  SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-  SPI_InitStructure.SPI_CRCPolynomial = 7;
-  SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-  SPI_Init(EncoderSensor_SPI, &SPI_InitStructure);
-
-  /* Enable SPI1  */
-  SPI_Cmd(EncoderSensor_SPI, ENABLE);
-
-  /* Configure GPIO PIN for Lis Chip select */
-  GPIO_InitStructure.GPIO_Pin = EncoderSensor_SPI_CS_PIN;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(EncoderSensor_SPI_CS_GPIO_PORT, &GPIO_InitStructure);
-
-  /* Deselect : Chip Select high */
-  GPIO_SetBits(EncoderSensor_SPI_CS_GPIO_PORT, EncoderSensor_SPI_CS_PIN);
-}
-
-/**
-  * @brief Read the abs encoder information and 2.4G signal via SPI interface.
-  *         Each time(10ms) only one channel data is read.
-  *         For total 16 data takes 160ms to refersh them all.
-  *         Channel 0~3 Steering Motor Position
-  *         Channel 4~7 Couplings Position
-  *         Channel 8~15 2.4G signal
-  * @param  vEncoderSensorRefersh: task name. 
-  *         pvParameters: task parameters.
-  * @retval None
-  */
-void vEncoderSensorRefershTask( void *pvParameters )
-{
-    portTickType xLastWakeTime;
-    uint8_t i = 0;
-    uint8_t SpiBuffer[2];
-    uint16_t  SpiBuffer16;
-
-    xLastWakeTime = xTaskGetTickCount();
-
-    for(;;)
-    {       
-      
-      if (i<15) {
-          i++;
-      } else {
-          i = 0;
-      }
-      
-      EncoderSensor_Read(SpiBuffer, i, 2);
-      SpiBuffer16 = (SpiBuffer[0]<<8) + SpiBuffer[1];
-      
-      if (i < 5){
-          SetSteeringMotorPosition((i), SpiBuffer16);
-      } else if (i < 8){
-          SetCouplingsPosition((i-4), SpiBuffer16);
-      } else {
-          SetRemoteControl((i-8), SpiBuffer16);
-      }
-      
-//    vDebugPrintf("i:%i, d:%i\n", i, SpiBuffer16);
-      vTaskDelayUntil( &xLastWakeTime, 10 / portTICK_RATE_MS );
-    }
-} 
-
-/**
-  * @brief  Reads a block of data from the EncoderSensor.
-  * @param  pBuffer : pointer to the buffer that receives the data read from the EncoderSensor.
-  * @param  ReadAddr : EncoderSensor's internal address to read from.
-  * @param  NumByteToRead : number of bytes to read from the EncoderSensor.
-  * @retval None
-  */
-static void EncoderSensor_Read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead)
-{  
-  /* Set chip select Low at the start of the transmission */
-  EncoderSensor_CS_LOW();
-  
-  /* Send the Address of the indexed register */
-  EncoderSensor_SendByte(ReadAddr);
-  
-  /* Receive the data that will be read from the device (MSB First) */
-  while(NumByteToRead > 0x00)
-  {
-    /* Send dummy byte (0x00) to generate the SPI clock to EncoderSensor (Slave device) */
-    *pBuffer = EncoderSensor_SendByte(DUMMY_BYTE);
-    NumByteToRead--;
-    pBuffer++;
-  }
-  
-  /* Set chip select High at the end of the transmission */ 
-  EncoderSensor_CS_HIGH();
-}
-
-/**
-  * @brief  Sends a Byte through the SPI interface and return the Byte received 
-  *         from the SPI bus.
-  * @param  Byte : Byte send.
-  * @retval The received byte value
-  */
-static uint8_t EncoderSensor_SendByte(uint8_t byte)
-{
-  /* Loop while DR register in not emplty */
-  EncoderSensorTimeout = EncoderSensor_FLAG_TIMEOUT;
-  while (SPI_I2S_GetFlagStatus(EncoderSensor_SPI, SPI_I2S_FLAG_TXE) == RESET)
-  {
-    if((EncoderSensorTimeout--) == 0) return EncoderSensor_TIMEOUT_UserCallback();
-  }
-  
-  /* Send a Byte through the SPI peripheral */
-  SPI_I2S_SendData(EncoderSensor_SPI, byte);
-  
-  /* Wait to receive a Byte */
-  EncoderSensorTimeout = EncoderSensor_FLAG_TIMEOUT;
-  while (SPI_I2S_GetFlagStatus(EncoderSensor_SPI, SPI_I2S_FLAG_RXNE) == RESET)
-  {
-    if((EncoderSensorTimeout--) == 0) return EncoderSensor_TIMEOUT_UserCallback();
-  }
-  
-  /* Return the Byte read from the SPI bus */
-  return (uint8_t)SPI_I2S_ReceiveData(EncoderSensor_SPI);
-}
+__IO uint32_t  SPIxTimeout = SPIx_FLAG_TIMEOUT;   
 
 #ifdef USE_DEFAULT_TIMEOUT_CALLBACK
+/*****************************************************************************/
 /**
-  * @brief  Basic management of the timeout situation.
-  * @param  None.
-  * @retval None.
-  */
-uint32_t EncoderSensor_TIMEOUT_UserCallback(void)
+*
+* SPI interface TIMEOUT UserCallback.
+*
+* @param	None
+*
+* @return	None
+*
+* @note		None
+*
+******************************************************************************/
+uint32_t SPIx_TIMEOUT_UserCallback(void)
 {
-	//printf("SPI interface ERROR!");
-  /* Block communication and all processes */
-  while (1)
-  {   
-  }
+	DebugPrintf("SPI interface TIMEOUT ERROR!");
+	/* Block communication and all processes */
+	while (1)
+	{   
+	}
 }
 #endif /* USE_DEFAULT_TIMEOUT_CALLBACK */
+
+/*****************************************************************************/
+/**
+*
+* Sends a Byte through the SPI interface and return the Byte received 
+*         	from the SPI bus.
+*
+* @param	Byte : Byte send.
+*
+* @return	The received byte value
+*
+* @note		None
+*
+******************************************************************************/
+static uint8_t SPIx_SendByte(uint8_t byte)
+{
+	/* Loop while DR register in not emplty */
+	SPIxTimeout = SPIx_FLAG_TIMEOUT;
+	while (SPI_I2S_GetFlagStatus(SPIx, SPI_I2S_FLAG_TXE) == RESET)
+	{
+		if((SPIxTimeout--) == 0) return SPIx_TIMEOUT_UserCallback();
+	}
+	
+	/* Send a Byte through the SPI peripheral */
+	SPI_I2S_SendData(SPIx, byte);
+	
+	/* Wait to receive a Byte */
+	SPIxTimeout = SPIx_FLAG_TIMEOUT;
+	while (SPI_I2S_GetFlagStatus(SPIx, SPI_I2S_FLAG_RXNE) == RESET)
+	{
+		if((SPIxTimeout--) == 0) return SPIx_TIMEOUT_UserCallback();
+	}
+	
+	/* Return the Byte read from the SPI bus */
+	return (uint8_t)SPI_I2S_ReceiveData(SPIx);
+}
+
+/*****************************************************************************/
+/**
+*
+* Reads a block of data from the SPI interface.
+*
+* @param	pBuffer : pointer to the buffer that receives the data 
+* 						read from the SPI interface.
+* @param	ReadAddr : Encoder's internal address to read from.
+* @param	NumByteToRead : number of bytes to read from the SPI interface.
+*
+* @return	None
+*
+* @note		None
+*
+******************************************************************************/
+static void SPIx_Read(uint8_t* pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead)
+{  
+	/* Set chip select Low at the start of the transmission */
+	SPIx_CS_LOW();
+  
+	/* Send the Address of the indexed register */
+	SPIx_SendByte(ReadAddr);
+	
+	/* Receive the data that will be read from the device (MSB First) */
+	while(NumByteToRead > 0x00)
+	{
+	/* Send dummy byte (0x00) to generate the SPI clock to Encoder (Slave device) */
+		*pBuffer = SPIx_SendByte(DUMMY_BYTE);
+		NumByteToRead--;
+		pBuffer++;
+	}
+	
+	/* Set chip select High at the end of the transmission */ 
+	SPIx_CS_HIGH();
+}
+
+/*****************************************************************************/
+/**
+*
+* Read the absolute encoder value and 2.4G remote control via SPI interface.
+* Each time only one channel data is read.
+* For total 16 data takes 160ms to refersh them all.
+* 	Channel 0~3 Steering Motor Position Encoder
+* 	Channel 4~7 Couplings Position Encoder
+* 	Channel 8~15 2.4G remote control
+*
+* @param	None
+*
+* @return	None
+*
+* @note		None
+*
+******************************************************************************/
+void EncoderRefershTask( void *pvParameters )
+{
+	portTickType xLastWakeTime;
+	uint8_t i = 0;
+	uint16_t SpiBuffer16;
+	uint8_t *pSpiBuffer = (uint8_t *)(&SpiBuffer16);
+	
+	xLastWakeTime = xTaskGetTickCount();
+	
+	for(;;)
+	{       
+	  
+		if (i<15) {i++;} 
+		else {i = 0;}
+		
+		SPIx_Read(pSpiBuffer, i, 2);
+		
+		if (i < 5){
+			SetSteeringMotorPosition((i), SpiBuffer16);
+		} else if (i < 8){
+			SetCouplingsPosition((i-4), SpiBuffer16);
+		} else {
+			SetRemoteControl((i-8), SpiBuffer16);
+		}
+	  
+//		vDebugPrintf("i:%i, d:%i\n", i, SpiBuffer16);
+		vTaskDelayUntil( &xLastWakeTime, 10 / portTICK_RATE_MS );
+	}
+} 
+
+/*****************************************************************************/
+/**
+*
+* Initialization the SPI Peripheral.
+*
+* @param	None
+*
+* @return	None
+*
+* @note		None
+*
+******************************************************************************/
+void EncoderInitialise(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	SPI_InitTypeDef  SPI_InitStructure;
+	
+	/* Enable the SPI periph */
+	RCC_APB1PeriphClockCmd(SPIx_CLK, ENABLE);
+	
+	/* Enable SCK, MOSI and MISO GPIO clocks */
+	RCC_AHB1PeriphClockCmd(SPIx_SCK_GPIO_CLK | SPIx_MISO_GPIO_CLK | SPIx_MOSI_GPIO_CLK, ENABLE);
+	
+	/* Enable CS  GPIO clock */
+	RCC_AHB1PeriphClockCmd(SPIx_CS_GPIO_CLK, ENABLE);
+	
+	GPIO_PinAFConfig(SPIx_SCK_GPIO_PORT, SPIx_SCK_SOURCE, SPIx_SCK_AF);
+	GPIO_PinAFConfig(SPIx_MISO_GPIO_PORT, SPIx_MISO_SOURCE, SPIx_MISO_AF);
+	GPIO_PinAFConfig(SPIx_MOSI_GPIO_PORT, SPIx_MOSI_SOURCE, SPIx_MOSI_AF);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	
+	/* SPI SCK pin configuration */
+	GPIO_InitStructure.GPIO_Pin = SPIx_SCK_PIN;
+	GPIO_Init(SPIx_SCK_GPIO_PORT, &GPIO_InitStructure);
+	
+	/* SPI  MOSI pin configuration */
+	GPIO_InitStructure.GPIO_Pin =  SPIx_MOSI_PIN;
+	GPIO_Init(SPIx_MOSI_GPIO_PORT, &GPIO_InitStructure);
+	
+	/* SPI MISO pin configuration */
+	GPIO_InitStructure.GPIO_Pin = SPIx_MISO_PIN;
+	GPIO_Init(SPIx_MISO_GPIO_PORT, &GPIO_InitStructure);
+	
+	/* SPI configuration -------------------------------------------------------*/
+	SPI_I2S_DeInit(SPIx);
+	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
+	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+	SPI_InitStructure.SPI_CRCPolynomial = 7;
+	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+	SPI_Init(SPIx, &SPI_InitStructure);
+	
+	/* Enable SPIx  */
+	SPI_Cmd(SPIx, ENABLE);
+	
+	/* Configure GPIO PIN for Lis Chip select */
+	GPIO_InitStructure.GPIO_Pin = SPIx_CS_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(SPIx_CS_GPIO_PORT, &GPIO_InitStructure);
+	
+	/* Deselect : Chip Select high */
+	GPIO_SetBits(SPIx_CS_GPIO_PORT, SPIx_CS_PIN);
+}
--- Demo/CORTEX_M4F_STM32F407ZG-SK/User/robot_encoder.h
+++ Demo/CORTEX_M4F_STM32F407ZG-SK/User/robot_encoder.h
@@ -1,103 +1,96 @@
+/*****************************************************************************/
 /**
-  ******************************************************************************
-  * @file    EncoderSensor.c
-  * @author  ZhouPeng
-  * @version V0.1
-  * @date    2013-3-22
-  * @brief   This file contains all the functions prototypes for the EncoderSensor.c
-  *          firmware driver.
-  */ 
+*
+* @file robot_encoder.h
+*
+*
+******************************************************************************/
 
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __ENCODERSENSOR_H
-#define __ENCODERSENSOR_H
+#ifndef ROBOT_ENCODER_H
+#define ROBOT_ENCODER_H
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
+/***************************** Include Files *********************************/
+ 
+ /* Debug Printf. */
+#include "robot_debug.h"
+
+/* ST Driver includes. */
+#include "stm32f4xx_conf.h"
    	 
+/************************** Constant Definitions *****************************/
 
-/**
-  * @}
-  */
-  
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Exported_Constants
-  * @{
-  */
 
-/* Uncomment the following line to use the default EncoderSensor_TIMEOUT_UserCallback() 
-   function implemented in stm32f4_discovery_EncoderSensor.c file.
-   EncoderSensor_TIMEOUT_UserCallback() function is called whenever a timeout condition 
+/* Uncomment the following line to use the default Encoder_TIMEOUT_UserCallback() 
+   function implemented in stm32f4_discovery_Encoder.c file.
+   Encoder_TIMEOUT_UserCallback() function is called whenever a timeout condition 
    occure during communication (waiting transmit data register empty flag(TXE)
    or waiting receive data register is not empty flag (RXNE)). */   
- #define USE_DEFAULT_TIMEOUT_CALLBACK 
+#define USE_DEFAULT_TIMEOUT_CALLBACK 
 
 /* Maximum Timeout values for flags waiting loops. These timeouts are not based
    on accurate values, they just guarantee that the application will not remain
    stuck if the SPI communication is corrupted.
    You may modify these timeout values depending on CPU frequency and application
    conditions (interrupts routines ...). */   
-#define EncoderSensor_FLAG_TIMEOUT         ((uint32_t)0x1000)
+#define SPIx_FLAG_TIMEOUT         ((uint32_t)0x1000)
 
 /**
-  * @brief  EncoderSensor SPI Interface pins
+  * @brief  SPI Interface pins
   */
-#define EncoderSensor_SPI                       SPI2
-#define EncoderSensor_SPI_CLK                   RCC_APB1Periph_SPI2
+#define SPIx                       SPI2
+#define SPIx_CLK                   RCC_APB1Periph_SPI2
 
-#define EncoderSensor_SPI_SCK_PIN               GPIO_Pin_13                  /* PB.13 */
-#define EncoderSensor_SPI_SCK_GPIO_PORT         GPIOB                       /* GPIOB */
-#define EncoderSensor_SPI_SCK_GPIO_CLK          RCC_AHB1Periph_GPIOB
-#define EncoderSensor_SPI_SCK_SOURCE            GPIO_PinSource13
-#define EncoderSensor_SPI_SCK_AF                GPIO_AF_SPI2
+#define SPIx_SCK_PIN               GPIO_Pin_13                  /* PB.13 */
+#define SPIx_SCK_GPIO_PORT         GPIOB                       /* GPIOB */
+#define SPIx_SCK_GPIO_CLK          RCC_AHB1Periph_GPIOB
+#define SPIx_SCK_SOURCE            GPIO_PinSource13
+#define SPIx_SCK_AF                GPIO_AF_SPI2
 
-#define EncoderSensor_SPI_MISO_PIN              GPIO_Pin_14                  /* PB.14 */
-#define EncoderSensor_SPI_MISO_GPIO_PORT        GPIOB                       /* GPIOB */
-#define EncoderSensor_SPI_MISO_GPIO_CLK         RCC_AHB1Periph_GPIOB
-#define EncoderSensor_SPI_MISO_SOURCE           GPIO_PinSource14
-#define EncoderSensor_SPI_MISO_AF               GPIO_AF_SPI1
+#define SPIx_MISO_PIN              GPIO_Pin_14                  /* PB.14 */
+#define SPIx_MISO_GPIO_PORT        GPIOB                       /* GPIOB */
+#define SPIx_MISO_GPIO_CLK         RCC_AHB1Periph_GPIOB
+#define SPIx_MISO_SOURCE           GPIO_PinSource14
+#define SPIx_MISO_AF               GPIO_AF_SPI1
 
-#define EncoderSensor_SPI_MOSI_PIN              GPIO_Pin_15                  /* PB.15 */
-#define EncoderSensor_SPI_MOSI_GPIO_PORT        GPIOB                       /* GPIOB */
-#define EncoderSensor_SPI_MOSI_GPIO_CLK         RCC_AHB1Periph_GPIOB
-#define EncoderSensor_SPI_MOSI_SOURCE           GPIO_PinSource15
-#define EncoderSensor_SPI_MOSI_AF               GPIO_AF_SPI1
+#define SPIx_MOSI_PIN              GPIO_Pin_15                  /* PB.15 */
+#define SPIx_MOSI_GPIO_PORT        GPIOB                       /* GPIOB */
+#define SPIx_MOSI_GPIO_CLK         RCC_AHB1Periph_GPIOB
+#define SPIx_MOSI_SOURCE           GPIO_PinSource15
+#define SPIx_MOSI_AF               GPIO_AF_SPI1
 
-#define EncoderSensor_SPI_CS_PIN                GPIO_Pin_12                  /* PB.12 */
-#define EncoderSensor_SPI_CS_GPIO_PORT          GPIOB                       /* GPIOB */
-#define EncoderSensor_SPI_CS_GPIO_CLK           RCC_AHB1Periph_GPIOB
+#define SPIx_CS_PIN                GPIO_Pin_12                  /* PB.12 */
+#define SPIx_CS_GPIO_PORT          GPIOB                       /* GPIOB */
+#define SPIx_CS_GPIO_CLK           RCC_AHB1Periph_GPIOB
 
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Exported_Macros
-  * @{
-  */
-#define EncoderSensor_CS_LOW()       GPIO_ResetBits(EncoderSensor_SPI_CS_GPIO_PORT, EncoderSensor_SPI_CS_PIN)
-#define EncoderSensor_CS_HIGH()      GPIO_SetBits(EncoderSensor_SPI_CS_GPIO_PORT, EncoderSensor_SPI_CS_PIN)
-/**
-  * @}
-  */ 
+/**************************** Type Definitions *******************************/
 
-/** @defgroup STM32F4_DISCOVERY_EncoderSensor_Exported_Functions
-  * @{
-  */ 
-void vEncoderSensorInitialise(void);
-void vEncoderSensorRefershTask( void *pvParameters );
+/***************** Macros (Inline Functions) Definitions *********************/
+
+#define SPIx_CS_LOW()       GPIO_ResetBits(SPIx_CS_GPIO_PORT, SPIx_CS_PIN)
+#define SPIx_CS_HIGH()      GPIO_SetBits(SPIx_CS_GPIO_PORT, SPIx_CS_PIN)
+
+/************************** Function Prototypes ******************************/
+
+void EncoderInitialise(void);
+void EncoderRefershTask( void *pvParameters );
 
 /* USER Callbacks: This is function for which prototype only is declared in
    MEMS accelerometre driver and that should be implemented into user applicaiton. */  
-/* EncoderSensor_TIMEOUT_UserCallback() function is called whenever a timeout condition 
+/* Encoder_TIMEOUT_UserCallback() function is called whenever a timeout condition 
    occure during communication (waiting transmit data register empty flag(TXE)
    or waiting receive data register is not empty flag (RXNE)).
    You can use the default timeout callback implementation by uncommenting the 
-   define USE_DEFAULT_TIMEOUT_CALLBACK in stm32f4_discovery_EncoderSensor.h file.
+   define USE_DEFAULT_TIMEOUT_CALLBACK in stm32f4_discovery_Encoder.h file.
    Typically the user implementation of this callback should reset MEMS peripheral
    and re-initialize communication or in worst case reset all the application. */
-uint32_t EncoderSensor_TIMEOUT_UserCallback(void);
+uint32_t SPIx_TIMEOUT_UserCallback(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __ENCODERSENSOR_H */
+#endif /* ROBOT_ENCODER_H */
